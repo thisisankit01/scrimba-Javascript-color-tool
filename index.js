4de68c3ca0a16924b11bcfeb730ecd770792072b
Ankit Pandey <ankitechzz@gmail.com>
2022-12-14 22:50:20 +0530

    done chapter 0

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
-
 const hexInput = document.getElementById("hexInput");
 const inputColor = document.getElementById("inputColor")
+const sliderText = document.getElementById("sliderText")
+const slider = document.getElementById("slider")
 
-hexInput.addEventListener('keyup', () => {
+
+hexInput.addEventListener('keyup', () => {  //keyup works with the real time of keyboard press
   const hex = hexInput.value;
    if(!isValidHex(hex)) return;
 
@@ -20,9 +21,54 @@ const isValidHex = (hex) => {
   return strippedHex.length === 3 || strippedHex.length === 6
 }
 
-console.log(isValidHex("#000000")) //true
-console.log(isValidHex("#0000000")) //false
-console.log(isValidHex("#ffffff")) //true
-console.log(isValidHex("#fff")) //true
-console.log(isValidHex("fff")) //true
-console.log(isValidHex("ac")) //false
+const convertHexToRgb = (hex) => {
+  if(!isValidHex(hex)) return null
+  
+  let strippedHex = hex.replace('#','')
+  if(strippedHex.length === 3) {
+    strippedHex = strippedHex[0]+strippedHex[0] + 
+    strippedHex[1]+strippedHex[1] + 
+    strippedHex[2]+strippedHex[2];  
+  
+  }
+    const r  = parseInt(strippedHex.substring(0,2), 16);
+    const g  = parseInt(strippedHex.substring(2,4), 16);
+    const b  = parseInt(strippedHex.substring(4,6), 16);
+    
+    return {r,g,b}
+
+}
+
+const convertRGBToHex = (r,g,b) => {
+  const firstPair = ("0" + r.toString(16)).slice(-2);
+  const secondPair = ("0" + g.toString(16)).slice(-2);
+  const thirdPair = ("0" + b.toString(16)).slice(-2);
+  
+  const hex = "#" + firstPair + secondPair + thirdPair;
+  return hex;
+}
+
+const alterColor = (hex, percentage) => {
+  const {r,g,b} = convertHexToRGB(hex);
+  
+  const amount = Math.floor((percentage/100) * 255);
+  
+  const newR = increaseWithin0To255(r,amount);
+  const newG = increaseWithin0To255(g,amount);
+  const newB = increaseWithin0To255(b,amount);
+  console.log(newR, newG, newB)
+  return convertRGBToHex(newR, newG, newB);
+}
+
+console.log(alterColor('fff', 10));
+
+const increaseWithin0To255 = (hex, amount) => {
+  const newHex = hex + amount;
+  if(newHex > 255) return 255;
+  if(newHex < 0) return 0;
+  return newHex;
+}
+
+slider.addEventListener('input', () => {
+  sliderText.textContent = `${slider.value}%`;
+})
